ce38fc89d052a6850be32717846271c544b467df
HelenArkh <alena.arkh@gmail.com>
2021-03-11 22:20:57 +0300

    Выполнение задания 1 модуля 13

---
+++ Module13Task1/Module13Task1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+
+namespace Module13Task1
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+           
+            // читаем весь файл в строку 
+            string text = File.ReadAllText(@"C:\Users\alena\Desktop\Text1.txt");
+            // разбиваем в массив, используя пробел в качестве разделителя
+            var words = text.Split(" ");
+
+            ListInsertPerfomance(words);
+            LinkedListInsertPerfomance(words);
+
+        }
+
+        static void ListInsertPerfomance(string[] words)
+        {
+
+            var listInsert = new List<string>();
+
+            // Запустим таймер
+            var watchTwo = Stopwatch.StartNew();
+
+            // Выполним вставку
+            listInsert.AddRange(words);
+
+            // Выведем результат
+            Console.WriteLine($"Вставка в List: {watchTwo.Elapsed.TotalMilliseconds}  мс");
+
+        }
+
+        static void LinkedListInsertPerfomance(string[] words)
+        {
+
+            var linkedListInsert = new LinkedList<string>();
+
+            // Запустим таймер
+            var watchTwo = Stopwatch.StartNew();
+
+
+            // Выполним вставку
+            foreach (string w in words)
+                linkedListInsert.AddFirst(w);
+
+            // Выведем результат
+            Console.WriteLine($"Вставка в LinkedList: {watchTwo.Elapsed.TotalMilliseconds}  мс");
+
+        }
+    }
+}
